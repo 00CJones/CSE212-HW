fa6d8fc6e5b560e4108d88bdc86e463edb6c0474
Calvin <chopin19@byui.edu>
2024-10-25 10:30:36 -0600

    finished the project for week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,7 +11,11 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data)
+        {
+            // No duplicates
+            return;
+        }
 
         if (value < Data)
         {
@@ -33,13 +37,50 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            if (Left is null)
+                return false;
+            else 
+                return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Check if the node has left and right pointers to null
+        if (Left == null && Right == null)
+        {
+            return 1; // Height of a single node tree is 1
+        }
+        int leftHeight;
+        int rightHeight;
+        // Recursive calls to calculate the height of left and right subtrees
+        if (Left != null)
+            leftHeight = Left.GetHeight();  // more nodes => keep going!
+        else
+            leftHeight = 0;                 // we reached the end of a branch
+        if (Right != null)
+            rightHeight = Right.GetHeight();
+        else
+            rightHeight = 0;
+
+        // Alternate shorthand way of doing this, but I preferred the above form so that I could better visualize what was happening.
+        // int leftHeight = Left?.GetHeight() ?? 0;    // this will set the value to 0 unless there is something to the left, in which case the method will be recursively called
+        // int rightHeight = Right?.GetHeight() ?? 0;
+
+        // Return the greater of the two heights + 1 (for the current node). This is how we keep track of the height as we go through the tree each time we reach the end of a branch.
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
